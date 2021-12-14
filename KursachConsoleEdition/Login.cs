b237968541c0950addc99d11ce0f25d006fc3810
satalurius <max99810@gmail.com>
2021-12-15 01:12:10 +0300

    Сделал логин (пока что общий для админа)

---
+++ KursachConsoleEdition/Login.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Newtonsoft.Json;
+
+
+namespace KursachConsoleEdition
+{
+    internal class Login
+    {
+        string path = "data/";
+
+        List<AdminLoginModel> GetLoginData()
+        {
+            var read = File.ReadAllText(path + "admin_login.json");
+            List<AdminLoginModel> json = JsonConvert.DeserializeObject<List<AdminLoginModel>>(read);
+            return json;
+        }
+
+        public bool CheckCorrectData(string login, string password)
+        {
+            var loginData = GetLoginData();
+
+            var loginInput = loginData.FirstOrDefault(x => x.Login == login && x.Password == password);
+
+            if (loginInput != null)
+            {
+                Console.WriteLine("Успешно");
+                return true;
+            }
+            return false;
+        }
+    }
+}
--- KursachConsoleEdition/Program.cs
+++ KursachConsoleEdition/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
 using System.IO;
-using System.Text.RegularExpressions;
+
 
 namespace KursachConsoleEdition
 {
@@ -15,130 +15,14 @@ namespace KursachConsoleEdition
                 dirInfo.Create();
             }
             //File.Create(dirInfo + "users_data.json");
- 
-            Menu();
+            //Console.WriteLine("Выберите роль");
+            //Console.WriteLine("Администратор");
+            //Console.WriteLine("Пользователь");
 
-            void Menu()
-            {
-                Admin admin = new Admin();
-                bool checkExit = false;
+            Menu menu = new Menu();
+            menu.LoginMenu();
+            menu.MenuMethod();
 
-                
-                while(!checkExit)
-                {
-                    Console.WriteLine("\nВыберите действие");
-                    Console.WriteLine("1 - просмотр пользователей");
-                    Console.WriteLine("2 - просмотр одного пользователя");
-                    Console.WriteLine("3 - изменить данные пользователя");
-                    Console.WriteLine("4 - удалить пользователя");
-                    Console.WriteLine("5 - добавить пользователя");
-                    Console.WriteLine("6 - выход");
-
-                    var item = Convert.ToInt32(Console.ReadLine());
-                    Console.WriteLine("\n");
-
-                    switch (item)
-                    {
-                        case 1:
-                            admin.ShowUsersData();
-                            break;
-                        case 2:
-                            bool checkId = false;
-                            while (!checkId)
-                            {
-                                if (!checkId)
-                                {
-                                    Console.WriteLine("Введите id пользователя");
-                                    int id = Convert.ToInt32(Console.ReadLine());
-                                    checkId = admin.ShowSingleUserData(id);
-                                }
-                            }
-                            break;
-                        case 3:
-                            checkId = false;
-                            while (!checkId)
-                            {
-                                if (!checkId)
-                                {
-                                    Console.WriteLine("Введите id пользователя");
-                                    int id = Convert.ToInt32(Console.ReadLine());
-                                    var userId = admin.CheckUser(id);
-                                    if (userId != null)
-                                    {
-                                        checkId = true;
-                                    }
-                                    var change = WhatToChangeInUser();
-                                    Console.WriteLine("Введите значение: ");
-                                    var textChange = Console.ReadLine();
-                                    admin.ChangeSingleUserData(id, change, textChange);
-                                }
-                            }
-                            break;
-                        case 4:
-                            checkId = false;
-                            while (!checkId)
-                            {
-                                if (!checkId)
-                                {
-                                    Console.WriteLine("Введите id пользователя");
-                                    int id = Convert.ToInt32(Console.ReadLine());
-                                    checkId = admin.DeleteSingleUserData(id);
-                                }
-                            }
-                            break;
-                        case 5:
-                            var data = InputToCreateUser();
-                            admin.CreateUser(data[0], data[1], data[2], data[3]);
-                            break;
-                        case 6:
-                        default:
-                            checkExit = true;
-                            break;
-                    }
-                }
-            }
-
-            int WhatToChangeInUser()
-            {
-                Console.WriteLine("Выберите, что изменить");
-                Console.WriteLine("1 - Имя");
-                Console.WriteLine("2 - Фамилия");
-                Console.WriteLine("3 - Адрес");
-                Console.WriteLine("4 - Номер телефона (8)");
-                int change = Convert.ToInt32(Console.ReadLine());
-                return change;
-            }
-            
-            string[] InputToCreateUser()
-            {
-                string[] data = new string[4];
-                Console.WriteLine("Имя: ");
-                data[0] = Console.ReadLine();
-                Console.WriteLine("Фамилия: ");
-                data[1] = Console.ReadLine();
-                Console.WriteLine("Адрес: ");
-                data[2] = Console.ReadLine();
-
-                bool checkNumber = false;
-
-                while(!checkNumber)
-                {
-                    Console.WriteLine("Номер телефона: ");
-                    data[3] = Console.ReadLine();
-
-                    if (!Regex.IsMatch(data[3], @"^((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}$"))
-                    {
-                        Console.WriteLine("Неправильно введен номер");
-                        Console.WriteLine("Повторите попытку");
-                       data[3] = Console.ReadLine();
-                    }
-                    else
-                        checkNumber = true;
-                }
-
-                return data;
-            }
-            
         }
     }
 }
